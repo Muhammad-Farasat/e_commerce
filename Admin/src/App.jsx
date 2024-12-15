064b78f0c55c35b73b44b5e257de447daa0593d5
Muhammad-Farasat <farasatkhan687@gmail.com>
2024-12-15 20:37:28 +0500

    commitment

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
     {/* <BrowserRouter> */}
       <Routes>
-        <Route path='/adminLogin' element={<Login /> } />
+        <Route path='/login' element={<Login /> } />
         <Route path='/*' element={<Admin />} />
       </Routes>
     {/* </BrowserRouter> */}
